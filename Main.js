f3579d2cc944902e0e06abfcf49a5af29e634b36
wjuniorw <wilson.junior90@gmail.com>
2021-06-30 11:42:17 -0300

    1 file modified

--- Main.js
+++ Main.js
@@ -1,37 +1,47 @@
-import React, { useState, useEffect } from 'react'
-import { connect } from 'react-redux'
-import Hello from './Hello'
-import './style.css'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Hello from './Hello';
+import './style.css';
 
-import { Creators as userActions } from './store/ducks/user'
-import { Creators as sessionActions } from './store/ducks/session'
+import { Creators as userActions } from './store/ducks/user';
+import { Creators as sessionActions } from './store/ducks/session';
 
-const Actions = { ...userActions, ...sessionActions }
+const Actions = { ...userActions, ...sessionActions };
 
-function Main({ loadUser, userLogin, logout, user }) {
-// function Main(props) {
-  // console.log('main props...', props)
-  console.log('user prop...', user)
-  useEffect(()=> {
-    loadUser()
-  }, [])
-    return (
-      <div >
-        {<Hello name={user.name} />}
-        <p>
-          Login and refresh page to see magic happen :)
-        </p>
-        <input type='button' style={{height: '30px'}} value="login" onClick={()=> userLogin()} />
-        <input type='button' style={{height: '30px'}} value="logout" onClick={()=> logout()} />
-      </div>
-    )
+function Main({ loadUser, userLogin, logout }) {
+  const dispatch = useDispatch();
+
+  const { name } = useSelector(state => state.user);
+
+  console.log('user prop...', user);
+  useEffect(() => {
+    dispatch(Actions.loadUser());
+  }, []);
+
+  return (
+    <div>
+      {<Hello name={name} />}
+      <p>Login and refresh page to see magic happen :)</p>
+      <input
+        type="button"
+        style={{ height: '30px' }}
+        value="login"
+        onClick={() => userLogin()}
+      />
+      <input
+        type="button"
+        style={{ height: '30px' }}
+        value="logout"
+        onClick={() => logout()}
+      />
+    </div>
+  );
 }
-
 
 export default connect(
   state => ({
     user: state.user,
-    session: state.session,
+    session: state.session
   }),
   Actions
-)(Main)
+)(Main);
